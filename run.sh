7c5ac82840b1867f4dcd27590cc023f6c5356bff
tomerun <tomerunmail@gmail.com>
2022-06-05 16:38:47 +0900

    update

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ for (( i = $SEED_START; i < $SEED_END; i++ )); do
 	seed=$(printf "%04d" $i)
 	echo "seed:$seed"
 	./main < in/"$seed".txt 2> "$seed".txt
-	tail -n 1 "$seed".txt >> log.txt
+	tail -n 2 "$seed".txt >> log.txt
 done
 
 aws s3 cp log.txt s3://marathon-tester/$RESULT_PATH/$(printf "%04d" $SEED_START).txt
